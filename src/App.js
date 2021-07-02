9f27128c060aecf3bb100c3089b3afcc14630ae1
Ukpeh Michael <marcelukpeh@gmail.com>
2021-07-02 10:49:43 +0100

    Changes being stashed

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import ExpenseIncome from './components/ExpenseIncome';
 import History from './components/History';
 import AddTransaction from './components/AddTransaction';
 import './App.css';
-import GlobalProvider , {GlobalContext}from './context/GlobalState';
 
 function App() {
   return (
   <>
  
-       <GlobalProvider>
+    
        <div className = "wrapper">
-       <Header />
        <Balance/>
        <ExpenseIncome/>
        <History/>
        <AddTransaction/>
        </div>
-       </GlobalProvider>
+      
       
   </>
   );
